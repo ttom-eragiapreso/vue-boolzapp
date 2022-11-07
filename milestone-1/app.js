b48871145c6327640ba27195b718625cde917b05
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-11-07 10:55:03 +0100

    init

---
+++ milestone-1/app.js
@@ -0,0 +1,20 @@
+const {createApp} = Vue;
+
+createApp({
+
+
+  data(){
+    return {
+
+    }
+  },
+
+  methods: {
+    
+  },
+
+  mounted(){
+    console.log("mounted")
+  }
+  
+}).mount("#app")
