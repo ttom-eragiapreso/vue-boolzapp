56c60d477fb568a18501854981d884a7ee233635
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-11-08 19:59:57 +0100

    finito con il bonus

--- milestone-1/app.js
+++ milestone-1/app.js
@@ -21,17 +21,20 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Hai portato a spasso il cane?",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Ricordati di stendere i panni",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 16:15:22",
               message: "Tutto fatto!",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         },
@@ -44,17 +47,20 @@ createApp({
             {
               date: "20/03/2020 16:30:00",
               message: "Ciao come stai?",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "20/03/2020 16:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             },
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             }
           ]
         },
@@ -66,17 +72,20 @@ createApp({
             {
               date: "28/03/2020 10:10:40",
               message: "La Marianna va in campagna",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             },
             {
               date: "28/03/2020 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         },
@@ -88,12 +97,14 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Si, ma preferirei andare al cinema",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         },
@@ -106,17 +117,20 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ricordati di chiamare la nonna",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Va bene, stasera la sento",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         },
@@ -129,17 +143,20 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao Claudia, hai novità?",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Non ancora",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:51:00",
               message: "Nessuna nuova, buona nuova",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             }
           ]
         },
@@ -152,12 +169,14 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         },
@@ -170,17 +189,20 @@ createApp({
             {
               date: "10/01/2020 15:30:55",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:50:00",
               message: "No, l'ho gia mangiata ieri, ordiniamo sushi!",
-              status: "sent"
+              status: "sent",
+              isOptionsVisible: false
             },
             {
               date: "10/01/2020 15:51:00",
               message: "OK!!",
-              status: "received"
+              status: "received",
+              isOptionsVisible: false
             }
           ]
         }
@@ -217,7 +239,8 @@ createApp({
       let userMsg = {
         date: `${today}/${month}/${year} ${hours}:${minutes}:00`,
         message: this.newMsg,
-        status: 'sent'
+        status: 'sent',
+        isOptionsVisible: false
       }
       
       this.contacts[this.activeConversation].messages.push(userMsg)
@@ -234,7 +257,8 @@ createApp({
         let aiMsg = {
           date: "07/11/2022 15:00:00",
           message: "ok!",
-          status: 'received'
+          status: 'received',
+          isOptionsVisible: false
         }
   
         this.contacts[this.activeConversation].messages.push(aiMsg)
@@ -253,11 +277,24 @@ createApp({
         this.contacts.forEach(contact => contact.visible = true )
       }
     },
-    // Funzione utility per prendere l'ultimo messaggio ricevuto da uno dei contatti per mostrarlo in preview.
+    // Funzione utility per prendere l'ultimo messaggio della chat e mostrarlo in preview. Soltanto fino a che la conversazione ha almeno un messaggio, altrimenti scrivo che la conversazione è vuota.
     getLastMsg(index){
 
-      let sentMsg = this.contacts[index].messages.filter(msg => msg.status === 'received')
-      return sentMsg[sentMsg.length -1].message
+      if(this.contacts[index].messages.length > 0){
+         return this.contacts[index].messages[this.contacts[index].messages.length -1].message
+      }else {
+        return `Questa conversazione è vuota`
+      }
+     
+    },
+    // Funzione che va a cancellare il messaggio sul quale si è cliccato
+    deleteMsg(index){
+
+      setTimeout(()=> {
+        this.contacts[this.activeConversation].messages[index].isOptionsVisible = false
+        this.contacts[this.activeConversation].messages.splice(index, 1);
+
+      }, 80)
     }
 
   }
